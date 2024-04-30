0f0cd4f7f7a9de36fb987fd20b39f9154b95a941
rajtirole <rtech23456@gmail.com>
2024-04-30 10:45:04 +0530

    added index file

--- src/constants/apiConstant.js
+++ src/constants/apiConstant.js
@@ -1,3 +1,2 @@
-export const BACKEND_URL='https://swiptory-backend-ka8m.onrender.com'
-// https://swiptory-backend-ka8m.onrender.com
+export const BACKEND_URL='https://swiptory-backend-ka8m.onrender.com/api/v1'
 export const FRONTEND_URL='https://swiptory-client.onrender.com'
