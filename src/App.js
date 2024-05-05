2d78f26d4392af4d94b4e1bbcbf50cac67047b1e
rajtirole <rtech23456@gmail.com>
2024-05-05 21:41:07 +0530

    story card changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BookmarkPage from "./pages/BookmarkPage";
 import StoryPage from "./pages/StoryPage";
@@ -9,35 +14,37 @@ import { usePostsContext } from "./context/PostContext";
 import UserCreatedPostsPage from "./pages/UserCreatedPostsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { FRONTEND_URL } from "./constants/apiConstant";
-console.log(FRONTEND_URL,'/userCreatedStory');
+console.log(FRONTEND_URL, "/userCreatedStory");
 const App = () => {
-  const {isLoading}=usePostsContext()
+  const { isLoading } = usePostsContext();
   return (
-    <main>
-      {isLoading&&<div class="loadingOverlay">
-  <div class="loader"></div>
-</div>
-}
-        <Routes >
-          {/* Route to the specific story page */}
-          <Route path="/" element={<HomePage />} />
-           {/* Route for 404 page */}
+    <>
+    
+    
+    
+    {isLoading && (
+        <div class="loadingOverlay">
+          <div class="loader"></div>
+        </div>
+      )}
+      <Routes>
+        {/* Route to the specific story page */}
+        <Route path="/" element={<HomePage />} />
+        {/* Route for 404 page */}
         <Route path="*" element={<NotFoundPage />} />
-          <Route path="/stories/:id" element={<StoryPage />} />
-          {/* <Route path={`${FRONTEND_URL}/userCreatedStory`} element={<UserCreatedPostsPage />} /> */}
-          <Route path="/userCreatedStory" element={<UserCreatedPostsPage />} />
+        <Route path="/stories/:id" element={<StoryPage />} />
+        {/* <Route path={`${FRONTEND_URL}/userCreatedStory`} element={<UserCreatedPostsPage />} /> */}
+        <Route path="/userCreatedStory" element={<UserCreatedPostsPage />} />
 
-          {/* Route to bookmark page */}
+        {/* Route to bookmark page */}
 
-          <Route path="/bookmarks" element={<BookmarkPage />} />
+        <Route path="/bookmarks" element={<BookmarkPage />} />
 
-          {/* Route to home page */}
-          
-         
-        </Routes>
-
+        {/* Route to home page */}
+      </Routes>
       <ToastContainer />
-    </main>
+      </>
+
   );
 };
 
